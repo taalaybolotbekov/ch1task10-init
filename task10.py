4accaf9f586c9aa960c40e15d4895c12b28521fd
taalaybolotbekov <bolotbekovtaalay@gmail.com>
2020-08-17 11:53:27 +0600

    chapter1 task10

---
+++ task10.py
@@ -0,0 +1,11 @@
+num1 = int(input('Введите первое число: '))
+num2 = int(input('Введите второе число: '))
+if num1 > num2:
+    print(num1, ' > ', num2)
+elif num1 < num2:
+    print(num1, ' < ', num2)
+else:
+    print('error')
+# python = 'Hi %s'
+# a = 'Python'
+# print(python % a)
